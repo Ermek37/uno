3c603fd512fbe80b0f7a4aecc8aeeb95a06cb717
acxcil <aeldaniiarovna@gmail.com>
2022-05-17 19:09:00 +0600

    class Card in uno.py

--- uno.py
+++ uno.py
@@ -7,3 +7,27 @@ rank = ('0','1','2','3','4','5','6','7','8','9','Skip','Reverse','Draw2','Draw4'
 ctype = {'0':'number','1':'number','2':'number','3':'number','4':'number','5':'number','6':'number',
             '7':'number','8':'number','9':'number','Skip':'action','Reverse':'action','Draw2':'action',
             'Draw4':'action_nocolor','Wild':'action_nocolor'}
+
+
+class Card:
+
+    def init(self, color, rank):
+        self.rank = rank
+        if ctype[rank] == 'number':
+            self.color = color
+            self.cardtype = 'number'
+        elif ctype[rank] == 'action':
+            self.color = color
+            self.cardtype = 'action'
+        else:
+            self.color = None
+            self.cardtype = 'action_nocolor'
+
+    def str(self):
+        if self.color == None:
+            return self.rank
+        else:
+            return self.color + " " + self.rank
+
+
+

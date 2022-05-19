0b5945718d07a748d45ad1ccf38546c304f0d237
acxcil <aeldaniiarovna@gmail.com>
2022-05-19 23:34:43 +0600

    disable translations method in bot.py

--- bot.py
+++ bot.py
@@ -487,5 +487,32 @@ def enable_translations(bot, update):
 
 
 
+@user_locale
+def disable_translations(bot, update):
+    """Handler for the /disable_translations command"""
+    chat = update.message.chat
+    user = update.message.from_user
+    games = gm.chatid_games.get(chat.id)
+
+    if not games:
+        send_async(bot, chat.id,
+                   text=_("There is no running game in this chat."))
+        return
+
+    game = games[-1]
+
+    if user.id in game.owner:
+        game.translate = False
+        send_async(bot, chat.id, text=_("Disabled multi-translations. "
+                                        "Enable them again with "
+                                        "/enable_translations"))
+        return
+
+    else:
+        send_async(bot, chat.id,
+                   text=_("Only the game creator ({name}) and admin can do that.")
+                   .format(name=game.starter.first_name),
+                   reply_to_message_id=update.message.message_id)
+        return
 
 

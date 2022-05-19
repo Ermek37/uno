25e187f3149dc9ec3ed9b2270694601be36e0845
Ermek Ilikeshova <ermek.ilikeshova_2025@ucentralasia.org>
2022-05-20 03:19:01 +0600

    timeout

---
+++ mwt.py
@@ -0,0 +1,16 @@
+import time
+
+class MWT(object):
+    _caches = {}
+    _timeouts = {}
+
+    def __init__(self,timeout=2):
+        self.timeout = timeout
+
+    def collect(self):
+        for func in self._caches:
+            cache = {}
+            for key in self._caches[func]:
+                if (time.time() - self._caches[func][key][1]) < self._timeouts[func]:
+                    cache[key] = self._caches[func][key]
+            self._caches[func] = cache

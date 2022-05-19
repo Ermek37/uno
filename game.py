b3c0b3324d4d03a165028e496767984df0a749ad
Ermek Ilikeshova <ermek.ilikeshova_2025@ucentralasia.org>
2022-05-19 21:03:32 +0600

    Properties: list of players, marks

--- game.py
+++ game.py
@@ -18,3 +18,37 @@ class Game(object):
     owner = ADMIN_LIST
     open = OPEN_LOBBY
     
+    def players(self):
+        players = list()
+        if not self.current_player:
+            return players
+
+        current_player = self.current_player
+        itplayer = current_player.next
+        players.append(current_player)
+        while itplayer and itplayer is not current_player:
+            players.append(itplayer)
+            itplayer = itplayer.next
+        return players
+
+    def start(self):
+        if self.mode == None or self.mode != "wild":
+            self.deck._fill_classic_()
+        else:
+            self.deck._fill_wild_()
+
+        self._first_card_()
+        self.started = True
+
+    def set_mode(self, mode):
+        self.mode = mode
+
+    def reverse(self):
+        self.reversed = not self.reversed
+
+    def turn(self):
+        self.logger.debug("Next Player")
+        self.current_player = self.current_player.next
+        self.current_player.drew = False
+        self.current_player.turn_started = datetime.now()
+        self.choosing_color = False

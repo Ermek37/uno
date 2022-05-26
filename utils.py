0d370777be3f78a4a5edf5e71316903603ce0e95
Ermek Ilikeshova <ermek.ilikeshova_2025@ucentralasia.org>
2022-05-26 22:30:17 +0600

    about the game

--- utils.py
+++ utils.py
@@ -87,5 +87,4 @@ def user_is_creator_or_admin(user, game, bot, chat):
 
 MWT(timeout=60*60)
 def get_admin_ids(bot, chat_id):
-    """Returns a list of admin IDs for a given chat. Results are cached for 1 hour."""
     return [admin.user.id for admin in bot.get_chat_administrators(chat_id)]

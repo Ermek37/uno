caf1a408dd2e76ac08cce7851a5d820c407d876a
Ermek Ilikeshova <ermek.ilikeshova_2025@ucentralasia.org>
2022-05-27 00:03:39 +0600

    about the game

--- results.py
+++ results.py
@@ -20,3 +20,36 @@ def add_choose_color(results, game):
                 InputTextMessageContent(display_color_group(color, game))
             )
         )
+def add_other_cards(player, results, game):
+    results.append(
+        InlineQueryResultArticle(
+            "hand",
+            title=_("Card (tap for game state):",
+                    "Cards (tap for game state):",
+                    len(player.cards)),
+            description=', '.join([repr(card) for card in player.cards]),
+            input_message_content=game_info(game)
+        )
+    )
+
+
+def player_list(game):
+    return [_("{name} ({number} card)",
+              "{name} ({number} cards)",
+              len(player.cards))
+            .format(name=player.user.first_name, number=len(player.cards))
+            for player in game.players]
+
+
+def add_no_game(results):
+
+    results.append(
+        InlineQueryResultArticle(
+            "nogame",
+            title=_("You are not playing"),
+            input_message_content=
+            InputTextMessageContent(_('Not playing right now. Use /new to '
+                                      'start a game or /join to join the '
+                                      'current game in this group'))
+        )
+    )
